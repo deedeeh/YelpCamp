4dad7c2323a6284576531b6180ead54bca999b12
deedeeh <dinahafez.8@gmail.com>
2019-02-19 00:09:48 +0000

    Created a show route and view, added a textarea in form and updated the create route

--- app.js
+++ app.js
@@ -12,7 +12,8 @@ app.use(express.static('./public'));
 //mongoose Schema setup
 const campgroundSchema = new mongoose.Schema({
   name: String,
-  image: String
+  image: String,
+  description: String
 });
 
 const Campground = mongoose.model("Campground", campgroundSchema);
@@ -28,7 +29,7 @@ app.get('/campgrounds', (req, res) => {
       console.log(err);
     } else {
       console.log('successfully showing data');
-      res.render('campgrounds', {campgrounds: Campgrounds});
+      res.render('index', {campgrounds: Campgrounds});
     }
   })
 });
@@ -40,8 +41,10 @@ app.get('/campgrounds/new', (req, res) => {
 app.post('/campgrounds', (req, res) => {
   const name = req.body.name;
   const image = req.body.image
+  const description = req.body.description
+  const newCampground = {name: name, image:image, description: description}
   //Create a new campground and save to db
-  Campground.create({name: name, image:image}, (err, newCampground) => {
+  Campground.create(newCampground, (err, newCampground) => {
     if(err) {
       console.log(err);
     } else {
@@ -50,5 +53,20 @@ app.post('/campgrounds', (req, res) => {
     }
   })
 });
+
+//show more info about a campground
+app.get('/campgrounds/:id', (req, res) => {
+  //find the campground with given id
+  Campground.findById(req.params.id, (err, foundCampground) => {
+    if(err) {
+      console.log(err);
+    } else {
+      //render show template with that campground
+      res.render('show', {campground: foundCampground});
+    }
+  });
+  
+  
+})
 
 app.listen(3000, console.log('YelpCamp has started!'));
