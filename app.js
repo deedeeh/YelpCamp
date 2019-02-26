6c73531b74526e2bd8a3ca7fbb4b3571f0062d37
deedeeh <dinahafez.8@gmail.com>
2019-02-26 01:18:58 +0000

    Added a seeds file with a function to remove all campgrounds, create new ones & working on comments

--- app.js
+++ app.js
@@ -3,9 +3,11 @@ const app = express();
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const Campground = require('./models/campground');
+const seedDB = require('./seeds')
 
+
+seedDB();
 mongoose.connect("mongodb://localhost/yelp_camp", { useNewUrlParser: true });
-
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static('./public'));
