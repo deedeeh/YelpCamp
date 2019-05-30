d747beca2da0fbf2724fb502efdb80636a8595bc
deedeeh <dinahafez.8@gmail.com>
2019-05-30 22:40:53 +0100

    Associated campground with user. Added isLoggedIn for the campground new and create routes.

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -2,6 +2,14 @@ const express     = require('express'),
       router      = express.Router(),
       Campground  = require('../models/campground');
 
+//check if user is logged in - middleware
+const isLoggedIn = (req, res, next) => {
+  if(req.isAuthenticated()) {
+    return next();
+  }
+  res.redirect('/login');
+}
+
 //INDEX
 router.get('/campgrounds', (req, res) => {
   //Get all campgrounds from db
@@ -16,16 +24,20 @@ router.get('/campgrounds', (req, res) => {
 });
 
 //NEW
-router.get('/campgrounds/new', (req, res) => {
+router.get('/campgrounds/new', isLoggedIn, (req, res) => {
   res.render('campgrounds/new');
 });
 
 //CREATE
-router.post('/campgrounds', (req, res) => {
+router.post('/campgrounds', isLoggedIn, (req, res) => {
   const name = req.body.name;
   const image = req.body.image
   const description = req.body.description
-  const newCampground = {name: name, image:image, description: description}
+  const author = {
+    id: req.user._id,
+    username: req.user.username
+  }
+  const newCampground = {name: name, image:image, description: description, author: author}
   //Create a new campground and save to db
   Campground.create(newCampground, (err, newCampground) => {
     if(err) {
@@ -49,13 +61,5 @@ router.get('/campgrounds/:id', (req, res) => {
     }
   });
 });
-
-//check if user is logged in - middleware
-const isLoggedIn = (req, res, next) => {
-  if(req.isAuthenticated()) {
-    return next();
-  }
-  res.redirect('/login');
-}
 
 module.exports = router;
